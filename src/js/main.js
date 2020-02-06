f25d684474252360dd660ff478ff18bf1eec3b58
Betrayer <StarDestroyer@ukr.net>
2020-02-06 14:58:49 +0200

    hw-08_done

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ const button = document.querySelector(".lightbox__button");
 const lightboxImg = document.querySelector(".lightbox__image");
 
 const imgList = images.reduce(
-  (acc, imgArr) =>
+  (acc, el) =>
     acc +
     `<li class="gallery__item">
 <a
@@ -17,29 +17,31 @@ const imgList = images.reduce(
 >
   <img
     class="gallery__image"
-    src=${imgArr.preview}
-    data-source=${imgArr.original}
-    alt=${imgArr.description}
+    src=${el.preview}
+    data-source=${el.original}
+    alt=${el.description}
   />
 </a>
 </li>`,
   ""
 );
+
 element.insertAdjacentHTML("afterbegin", imgList);
 
 element.addEventListener("click", mouseClick);
 
 function mouseClick(e) {
   if (e.target.classList.contains("gallery__image")) {
-    lightbox.classList.toggle("is-open");
-    lightboxImg.src = e.target.dataset.source;
+    lightbox.classList.add("is-open");
+    lightboxImg.setAttribute("src", e.target.dataset.source);
+    button.addEventListener("click", closeImg);
   }
-  button.addEventListener("click", closeImg);
 }
 
 function closeImg(e) {
   if (e.target.classList.contains("lightbox__button")) {
-    lightbox.classList.toggle("is-open");
+    lightbox.classList.remove("is-open");
+    lightboxImg.removeAttribute("src");
   }
   button.removeEventListener("click", closeImg);
 }

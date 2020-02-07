41d33f14c864c2038bf22d5fdc34d5dab9da4d3e
Betrayer <StarDestroyer@ukr.net>
2020-02-07 10:42:47 +0200

    hw-08_added_some_features

--- src/js/main.js
+++ src/js/main.js
@@ -34,14 +34,33 @@ function mouseClick(e) {
   if (e.target.classList.contains("gallery__image")) {
     lightbox.classList.add("is-open");
     lightboxImg.setAttribute("src", e.target.dataset.source);
-    button.addEventListener("click", closeImg);
+    // button.addEventListener("click", closeImg);
+    document.addEventListener("keydown", closeModal);
+    lightbox.addEventListener("click", smth);
   }
 }
 
-function closeImg(e) {
-  if (e.target.classList.contains("lightbox__button")) {
+// function closeImg(e) {
+//   if (e.target.classList.contains("lightbox__button")) {
+//     lightbox.classList.remove("is-open");
+//     lightboxImg.removeAttribute("src");
+//   }
+//   button.removeEventListener("click", closeImg);
+// }
+
+function smth(e) {
+  if (!e.target.classList.contains("lightbox__image")) {
     lightbox.classList.remove("is-open");
     lightboxImg.removeAttribute("src");
+    document.removeEventListener("keydown", closeModal);
+    // button.removeEventListener("click", closeImg);
   }
-  button.removeEventListener("click", closeImg);
+}
+
+function closeModal(e) {
+  if (e.keyCode === 27) {
+    lightbox.classList.remove("is-open");
+    document.removeEventListener("keydown", closeModal);
+    // button.removeEventListener("click", closeImg);
+  }
 }
